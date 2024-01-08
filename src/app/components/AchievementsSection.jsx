bf527c665c78ed1fe499cb29c9532fd47cb7618e
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2024-01-08 23:45:53 +0000

    sorting spacing on website

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -95,14 +95,14 @@ const AchievementsSection = () => {
 
   return (
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-      <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
+      <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:gap-x-4 sm:flex-row items-center justify-between">
         {achievementsList.map((achievement, index) => {
           return (
             <div
               key={index}
-              className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
+              className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0 sm:mx-0"
             >
-              <h2 className="text-white text-4xl font-bold flex flex-row">
+              <h2 className="text-white text-4xl font-bold sm:text-lg md:text-xl lg:text-4xl flex flex-row">
                 {achievement.prefix}
                 <SpringNumber n={achievement.value}/>
                 {/* {achievement.postfix} */}
