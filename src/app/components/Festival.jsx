1f7d7c2aa56feb3bf993cf226c80b44111763b5d
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2024-02-14 17:41:13 +0000

    audio position

--- src/app/components/Festival.jsx
+++ src/app/components/Festival.jsx
@@ -130,7 +130,7 @@ function Festival() {
       key={index}
       src={hoveredIndex === index ? pos.iconSrcActive : pos.iconSrc}
       alt={pos?.modalTitle}
-      className={`${ pos.modalTitle !== 'manByBar' && 'custom-bounce'}`}
+      className={`${ pos.modalTitle !== 'manByBar' && 'image'}`}
       width={
         pos.modalTitle === 'manByBar' || pos.modalTitle === 'lapDance' ? (
           widowSize <= 480 ? 45 : 
@@ -172,47 +172,10 @@ function Festival() {
           95
         )
       }
-      height={
-        pos.modalTitle === 'manByBar' || pos.modalTitle === 'lapDance' ? (
-          widowSize <= 480 ? 45 : 
-          widowSize <= 520 ? 65 :
-          widowSize <= 560 ? 70 :
-          widowSize <= 600 ? 73 :
-          widowSize <= 640 ? 80 : 
-          widowSize <= 680 ? 83 :
-          widowSize <= 720 ? 84 :
-          widowSize <= 760 ? 85 :
-          widowSize <= 768 ? 90 :
-          widowSize <= 800 ? 95 :
-          widowSize <= 840 ? 95 :
-          widowSize <= 880 ? 95 :
-          widowSize <= 920 ? 100 :
-          widowSize <= 960 ? 110 :
-          widowSize <= 1000 ? 120 :
-          widowSize <= 1040 ? 130 :
-          widowSize <= 1080 ? 140 :
-          150
-        ) : (
-          widowSize <= 480 ? 28 :
-          widowSize <= 520 ? 32 :
-          widowSize <= 560 ? 36 :
-          widowSize <= 600 ? 40 :
-          widowSize <= 640 ? 45 : 
-          widowSize <= 680 ? 50 :
-          widowSize <= 720 ? 55 :
-          widowSize <= 760 ? 60 :
-          widowSize <= 768 ? 60 :
-          widowSize <= 800 ? 65 :
-          widowSize <= 840 ? 67 :
-          widowSize <= 880 ? 70 :
-          widowSize <= 920 ? 72 :
-          widowSize <= 960 ? 80 :
-          widowSize <= 1000 ? 90 :
-          widowSize <= 1040 ? 92 :
-          widowSize <= 1080 ? 94 :
-          95
-        )
-      }    />
+      height={90}
+      // style={{ height: 'auto' }} // Example inline styles
+
+     />
   </div>
 ))}
 
@@ -230,7 +193,7 @@ function Festival() {
           </button>
         </div>
       </Modal>
-      <audio controls autoplay loop preload className='relative bottom-0 left-0 w-32'>
+      <audio controls autoplay loop preload className='fixed bottom-0 left-0 w-32'>
   <source src="audio/shakeshake-song.mp4" type="audio/mp4" />
   Your browser does not support the audio element.
 </audio>
