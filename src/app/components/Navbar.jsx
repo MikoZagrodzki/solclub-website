68510111f25c3ee3403cab23c20e648642e73849
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2024-02-16 16:11:21 +0000

    icons navbar - shake icon added to right

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -39,12 +39,12 @@ const Navbar = () => {
   return (
     <nav className={`fixed mx-auto top-0 left-0 right-0  z-20 ${navbarOpen&&' border-b-2 bg-black bg-opacity-30'}`}>
       <div className=' flex flex-row-reverse container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2'>
-        <div className='w-1/3 flex justify-end'>
+        {/* <div className='w-1/3 flex justify-end'> */}
           {/* <button className=' bg-white rounded-md px-1 hover:bg-[#F9EAF9]'>
             <Image src={connectWallet} alt='Connnect Wallet' width={100} height={100} className='shadow-xl '/>
           </button> */}
-        </div>
-        <div className='w-1/3 flex justify-center'>
+        {/* </div> */}
+        <div className='w-1/3 flex justify-end'>
         <Link href={'/'} className='w-1/3 text-2xl md:text-5xl text-white font-semibold flex items-center justify-center gap-1 hover:scale-105'>
           <Image src='/icons/shake-icon.svg' alt='hero image' width={80} height={80} className='' />
         </Link>
