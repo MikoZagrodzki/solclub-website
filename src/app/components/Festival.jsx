4188215e4da184936716fb53635b4984b2e63b0f
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2024-02-20 21:57:23 +0000

    added pdf and faq

--- src/app/components/Festival.jsx
+++ src/app/components/Festival.jsx
@@ -5,13 +5,12 @@ import './Festival.module.css'; // Import a CSS file for styles
 import imageMapResize from './imageMapResizer';
 import Modal from 'react-modal';
 import Link from 'next/link';
-import neonColor from '../../styles/neon.module.css'
-import speakerAnimation from "../../styles/speakerAnimation.module.css"
-import iconsAnimation from "../../styles/iconsAnimation.module.css"
-
+import neonColor from '../../styles/neon.module.css';
+import speakerAnimation from '../../styles/speakerAnimation.module.css';
+import iconsAnimation from '../../styles/iconsAnimation.module.css';
+import gradientBg from '../../styles/gradientBg.module.css';
 
 function Festival() {
-  
   const [isSmallScreen, setIsSmallScreen] = useState(false);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -22,7 +21,8 @@ function Festival() {
   const [widowSize, setWindowSize] = useState(null);
   const [isMusicPlaying, setIsMusicPlaying] = useState(false);
 
-
+  const [isFaqModalOpen, setIsFaqModalOpen] = useState(false);
+  const [isFaqHovered, setIsFaqHovered] = useState(false);
 
   const audioRef = useRef(null);
 
@@ -34,7 +34,6 @@ function Festival() {
       audioRef.current.pause();
     }
   };
-
 
   //THIS CHANGES BACKGROUND TO DIFFERENT IMAGE ON SMALL SCREEN
   useEffect(() => {
@@ -57,25 +56,88 @@ function Festival() {
   }, []);
 
   //THIS HELPS TO KEEP TRACK OF THE IMAGE MAP IN CASE OF IMAGE RESOLUTION
-  const iconStyle = (x, y) =>
-      ({  position: 'absolute',
-          top: `${(y / 720) * 100}%`,
-          left: `${(x / 1080) * 100}%`,
-          transform: 'translate(-50%, -50%)', // Center the icon on the coordinates
-        });
+  const iconStyle = (x, y) => ({
+    position: 'absolute',
+    top: `${(y / 720) * 100}%`,
+    left: `${(x / 1080) * 100}%`,
+    transform: 'translate(-50%, -50%)', // Center the icon on the coordinates
+  });
 
   //HERE YOU CAN ADD MORE MAP POINTS TO THE IMAGE
   const iconsPositions = [
-        { x: 380, y: 415, modalTitle:'bot', modalSvgSrc:"/modalContent/shake-bot.svg",  iconSrcActive:"/pinIcons/shake-bot-active.svg", iconSrc: '/pinIcons/shake-bot.svg',  class:''},
-        { x: 630, y: 325, modalTitle:'cards', modalSvgSrc:"/modalContent/shake-cards.svg", iconSrcActive:"/pinIcons/shake-cards-active.svg", iconSrc: '/pinIcons/shake-cards.svg', class:''},
-        { x: 260, y: 400, modalTitle:'lottery', modalSvgSrc:"/modalContent/shake-lottery.svg", iconSrcActive:"/pinIcons/shake-lottery-active.svg", iconSrc: '/pinIcons/shake-lottery.svg', class:'' },
-        { x: 501, y: 385, modalTitle:'nft', modalSvgSrc:"/modalContent/shake-nft.svg",  iconSrcActive:"/pinIcons/shake-nft-active.svg", iconSrc: '/pinIcons/shake-nft.svg', class:'z-0' },
-        { x: 500, y: 600, modalTitle:'buy', modalSvgSrc:"/modalContent/shake-buy.svg", iconSrcActive:"/pinIcons/shake-buy-active.svg", iconSrc: '/pinIcons/shake-buy.svg',  class: 'z-40'},
-        { x: 950, y: 350, modalTitle:'paper', modalSvgSrc:"/modalContent/shake-paper.svg", iconSrcActive:"/pinIcons/shake-paper-active.svg", iconSrc: '/pinIcons/shake-paper.svg',class:'' },
-        { x: 1000, y: 500, modalTitle: 'lapDance', modalSvgSrc: "/modalContent/shake-lapDance.svg", iconSrcActive: "/pinIcons/shake-dance-active.png", iconSrc: '/pinIcons/shake-dance.png', class: '' },
-        { x: 385, y: 605, modalTitle:'manByBar', modalSvgSrc:"", iconSrcActive:"/pinIcons/shake-manByBar.svg", iconSrc: '/pinIcons/shake-manByBar.svg', class:'' },
-
-      ];
+    {
+      x: 380,
+      y: 415,
+      modalTitle: 'bot',
+      modalSvgSrc: '/modalContent/shake-bot.svg',
+      iconSrcActive: '/pinIcons/shake-bot-active.svg',
+      iconSrc: '/pinIcons/shake-bot.svg',
+      class: '',
+    },
+    {
+      x: 630,
+      y: 325,
+      modalTitle: 'cards',
+      modalSvgSrc: '/modalContent/shake-cards.svg',
+      iconSrcActive: '/pinIcons/shake-cards-active.svg',
+      iconSrc: '/pinIcons/shake-cards.svg',
+      class: '',
+    },
+    {
+      x: 260,
+      y: 400,
+      modalTitle: 'lottery',
+      modalSvgSrc: '/modalContent/shake-lottery.svg',
+      iconSrcActive: '/pinIcons/shake-lottery-active.svg',
+      iconSrc: '/pinIcons/shake-lottery.svg',
+      class: '',
+    },
+    {
+      x: 501,
+      y: 385,
+      modalTitle: 'nft',
+      modalSvgSrc: '/modalContent/shake-nft.svg',
+      iconSrcActive: '/pinIcons/shake-nft-active.svg',
+      iconSrc: '/pinIcons/shake-nft.svg',
+      class: 'z-0',
+    },
+    {
+      x: 500,
+      y: 600,
+      modalTitle: 'buy',
+      modalSvgSrc: '/modalContent/shake-buy.svg',
+      iconSrcActive: '/pinIcons/shake-buy-active.svg',
+      iconSrc: '/pinIcons/shake-buy.svg',
+      class: 'z-40',
+    },
+    {
+      x: 950,
+      y: 350,
+      modalTitle: 'paper',
+      modalSvgSrc: '/modalContent/shake-paper.svg',
+      iconSrcActive: '/pinIcons/shake-paper-active.svg',
+      iconSrc: '/pinIcons/shake-paper.svg',
+      class: '',
+    },
+    {
+      x: 1000,
+      y: 500,
+      modalTitle: 'lapDance',
+      modalSvgSrc: '/modalContent/shake-lapDance.svg',
+      iconSrcActive: '/pinIcons/shake-dance-active.png',
+      iconSrc: '/pinIcons/shake-dance.png',
+      class: '',
+    },
+    {
+      x: 385,
+      y: 605,
+      modalTitle: 'manByBar',
+      modalSvgSrc: '',
+      iconSrcActive: '/pinIcons/shake-manByBar.svg',
+      iconSrc: '/pinIcons/shake-manByBar.svg',
+      class: '',
+    },
+  ];
 
   //FUNCTIONS FOR THE POPUP
   const openModal = (pin) => {
@@ -108,50 +170,63 @@ function Festival() {
     },
   };
 
+  const modalFaqStyles = {
+    content: {
+      backgroundColor: 'transparent',
+      border: 'none',
+      // maxWidth: '600px',
+      minHeight: '100vh',
+      width: '100%',
+      overflow: 'hidden',
+    },
+    overlay: {
+      backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    },
+  };
+
   //PDF OPEN FUNCTION
   const openPdfInNewTab = (pdfUrl) => {
     // Open the PDF in a new tab
     const newWindow = window.open(pdfUrl, '_blank');
-  
+
     // Focus on the new tab (optional)
     if (newWindow) {
       newWindow.focus();
     }
   };
 
-    const getRandomDelayFactor = () => Math.random();
+  const getRandomDelayFactor = () => Math.random();
 
-    const [isSrcActive, setIsSrcActive] = useState({});
-    const [randomDelays, setRandomDelays] = useState({});
-  
-    useEffect(() => {
-      // Calculate random delays for each image
-      const delays = {};
-      iconsPositions.forEach((_, index) => {
-        delays[index] = Math.random() * 20000 + 5000; // Adjust the maximum delay time (in milliseconds) as needed and add a base delay of 5000 ms
-      });
-      setRandomDelays(delays);
-  
-      // Set up intervals with random delays
-      const intervals = iconsPositions.map((_, index) =>
-        setInterval(() => {
-          // Toggle the state for the current image index twice to create flicker effect
-          setIsSrcActive(prev => ({ ...prev, [index]: !prev[index] }));
-          setTimeout(() => {
-            setIsSrcActive(prev => ({ ...prev, [index]: !prev[index] }));
-          }, 300); // Adjust the duration of each flicker as needed (300ms for example)
-        }, delays[index])
-      );
-  
-      return () => {
-        // Clean up intervals on component unmount
-        intervals.forEach(interval => clearInterval(interval));
-      };
-    }, []); // Empty dependency array to run effect only once
-  
-  
+  const [isSrcActive, setIsSrcActive] = useState({});
+  const [randomDelays, setRandomDelays] = useState({});
+
+  useEffect(() => {
+    // Calculate random delays for each image
+    const delays = {};
+    iconsPositions.forEach((_, index) => {
+      delays[index] = Math.random() * 20000 + 5000; // Adjust the maximum delay time (in milliseconds) as needed and add a base delay of 5000 ms
+    });
+    setRandomDelays(delays);
+
+    // Set up intervals with random delays
+    const intervals = iconsPositions.map((_, index) =>
+      setInterval(() => {
+        // Toggle the state for the current image index twice to create flicker effect
+        setIsSrcActive((prev) => ({ ...prev, [index]: !prev[index] }));
+        setTimeout(() => {
+          setIsSrcActive((prev) => ({ ...prev, [index]: !prev[index] }));
+        }, 300); // Adjust the duration of each flicker as needed (300ms for example)
+      }, delays[index])
+    );
+
+    return () => {
+      // Clean up intervals on component unmount
+      intervals.forEach((interval) => clearInterval(interval));
+    };
+  }, []); // Empty dependency array to run effect only once
+
   return (
-    <div className='w-full h-full relative '>
+    <div className='w-full h-full relative'>
       <div className='flex justify-center container '>
         <img useMap='#image_map' src='/images/shake-bg-svg.svg' alt='Shake Background' width={1080} height={720} className='' />
         <map name='image_map'>
@@ -165,75 +240,122 @@ function Festival() {
         </map>
       </div>
       {iconsPositions.map((pos, index) => (
-  <div
-    key={index}
-    style={iconStyle(pos.x, pos.y)}
-    className={`cursor-pointer`}
-    onClick={pos.modalTitle !== 'manByBar' ? () => {openModal(pos)} : null}
-    onMouseEnter={pos.modalTitle !== 'manByBar' ? () => setHoveredIndex(index) : null}
-    onMouseLeave={pos.modalTitle !== 'manByBar' ? () => setHoveredIndex(null) : null}
-  >
-    <Image
-      key={index}
-      src={hoveredIndex === index ? pos.iconSrcActive : isSrcActive[index] ? pos.iconSrcActive : pos.iconSrc}
-
-
-      alt={pos?.modalTitle}
-      className={`${pos.modalTitle !== 'manByBar' ? iconsAnimation.jumpAnimation : ''} `}
-      style={{ '--delay-factor': getRandomDelayFactor() }}
-      width={
-        pos.modalTitle === 'manByBar' || pos.modalTitle === 'lapDance' ? (
-          widowSize <= 480 ? 45 : 
-          widowSize <= 520 ? 65 :
-          widowSize <= 560 ? 70 :
-          widowSize <= 600 ? 73 :
-          widowSize <= 640 ? 80 : 
-          widowSize <= 680 ? 83 :
-          widowSize <= 720 ? 84 :
-          widowSize <= 760 ? 85 :
-          widowSize <= 768 ? 90 :
-          widowSize <= 800 ? 95 :
-          widowSize <= 840 ? 95 :
-          widowSize <= 880 ? 95 :
-          widowSize <= 920 ? 100 :
-          widowSize <= 960 ? 110 :
-          widowSize <= 1000 ? 120 :
-          widowSize <= 1040 ? 130 :
-          widowSize <= 1080 ? 140 :
-          150
-        ) : (
-          widowSize <= 480 ? 28 :
-          widowSize <= 520 ? 32 :
-          widowSize <= 560 ? 36 :
-          widowSize <= 600 ? 40 :
-          widowSize <= 640 ? 45 : 
-          widowSize <= 680 ? 50 :
-          widowSize <= 720 ? 55 :
-          widowSize <= 760 ? 60 :
-          widowSize <= 768 ? 60 :
-          widowSize <= 800 ? 65 :
-          widowSize <= 840 ? 67 :
-          widowSize <= 880 ? 70 :
-          widowSize <= 920 ? 72 :
-          widowSize <= 960 ? 80 :
-          widowSize <= 1000 ? 90 :
-          widowSize <= 1040 ? 92 :
-          widowSize <= 1080 ? 94 :
-          95
-        )
-      }
-      height={90}
-      // style={{ height: 'auto' }} // Example inline styles
-
-     />
-  </div>
-))}
-
+        <div
+          key={index}
+          style={iconStyle(pos.x, pos.y)}
+          className={`cursor-pointer`}
+          onClick={
+            pos.modalTitle !== 'manByBar'
+              ? () => {
+                  openModal(pos);
+                }
+              : null
+          }
+          onMouseEnter={pos.modalTitle !== 'manByBar' ? () => setHoveredIndex(index) : null}
+          onMouseLeave={pos.modalTitle !== 'manByBar' ? () => setHoveredIndex(null) : null}
+        >
+          <Image
+            key={index}
+            src={hoveredIndex === index ? pos.iconSrcActive : isSrcActive[index] ? pos.iconSrcActive : pos.iconSrc}
+            alt={pos?.modalTitle}
+            className={`${pos.modalTitle !== 'manByBar' ? iconsAnimation.jumpAnimation : ''} `}
+            style={{ '--delay-factor': getRandomDelayFactor() }}
+            width={
+              pos.modalTitle === 'manByBar' || pos.modalTitle === 'lapDance'
+                ? widowSize <= 480
+                  ? 45
+                  : widowSize <= 520
+                  ? 65
+                  : widowSize <= 560
+                  ? 70
+                  : widowSize <= 600
+                  ? 73
+                  : widowSize <= 640
+                  ? 80
+                  : widowSize <= 680
+                  ? 83
+                  : widowSize <= 720
+                  ? 84
+                  : widowSize <= 760
+                  ? 85
+                  : widowSize <= 768
+                  ? 90
+                  : widowSize <= 800
+                  ? 95
+                  : widowSize <= 840
+                  ? 95
+                  : widowSize <= 880
+                  ? 95
+                  : widowSize <= 920
+                  ? 100
+                  : widowSize <= 960
+                  ? 110
+                  : widowSize <= 1000
+                  ? 120
+                  : widowSize <= 1040
+                  ? 130
+                  : widowSize <= 1080
+                  ? 140
+                  : 150
+                : widowSize <= 480
+                ? 28
+                : widowSize <= 520
+                ? 32
+                : widowSize <= 560
+                ? 36
+                : widowSize <= 600
+                ? 40
+                : widowSize <= 640
+                ? 45
+                : widowSize <= 680
+                ? 50
+                : widowSize <= 720
+                ? 55
+                : widowSize <= 760
+                ? 60
+                : widowSize <= 768
+                ? 60
+                : widowSize <= 800
+                ? 65
+                : widowSize <= 840
+                ? 67
+                : widowSize <= 880
+                ? 70
+                : widowSize <= 920
+                ? 72
+                : widowSize <= 960
+                ? 80
+                : widowSize <= 1000
+                ? 90
+                : widowSize <= 1040
+                ? 92
+                : widowSize <= 1080
+                ? 94
+                : 95
+            }
+            height={90}
+            // style={{ height: 'auto' }} // Example inline styles
+          />
+        </div>
+      ))}
 
       {/* Modal */}
       <Modal isOpen={isModalOpen} onRequestClose={closeModal} style={modalStyles} contentLabel='Pin Modal' className=''>
         <div className='container flex flex-col items-center gap-2 relative'>
           <Image src={selectedPin?.modalSvgSrc} alt={selectedPin?.modalTitle} width={600} height={300} title={selectedPin?.modalTitle} className='' />
+          {selectedPin?.modalTitle === 'paper' && (
+            <button
+              onClick={() => openPdfInNewTab('/pdf/shake-paper-pdf.pdf')}
+              className={`cursor-pointer flex flex-col items-center absolute ${isSmallScreen ? 'bottom-8' : 'bottom-20'} left-50 z-10`}
+              >
+              <Image
+                src={'/modalContent/shake-paper-pdf.svg'}
+                width={80}
+                height={80}
+                className={`  hover:scale-105 hover:brightness-125`}
+              />
+            </button>
+          )}
           <button
             className='absolute top-0 right-0 bg-gradient-radial   from-[#E664BE]  to-[#E66484] px-2 rounded-md cursor-pointer text-white shadow-xl hover:filter hover:brightness-125'
             onClick={closeModal}
@@ -247,8 +369,8 @@ function Festival() {
       <div className='fixed bottom-0 left-0 w-32'>
         {/* PNG Image */}
         <img
-          src="/icons/shake-speaker.svg"
-          alt="PNG Image"
+          src='/icons/shake-speaker.svg'
+          alt='PNG Image'
           onClick={toggleMusic}
           className={`${speakerAnimation.pngImage} ${isMusicPlaying ? speakerAnimation.jumpingMore : speakerAnimation.jumpingLess}  `}
           width={70}
@@ -257,10 +379,40 @@ function Festival() {
 
         {/* Audio Player */}
         <audio ref={audioRef} loop preload>
-          <source src="audio/shakeshake-song.mp4" type="audio/mp4" />
+          <source src='audio/shakeshake-song.mp4' type='audio/mp4' />
           Your browser does not support the audio element.
         </audio>
       </div>
+      <button
+        onClick={() => setIsFaqModalOpen(true)}
+        onMouseEnter={() => setIsFaqHovered(true)}
+        onMouseLeave={() => setIsFaqHovered(false)}
+        className='fixed bottom-2 right-2'
+      >
+        <Image
+          src={isFaqHovered ? '/icons/shake-faq-active.svg' : '/icons/shake-faq.svg'}
+          width={isSmallScreen ? 27 : 45}
+          height={isSmallScreen ? 27 : 45}
+          className='w-32 hover:scale-105 cursor-pointer'
+        />
+      </button>
+      <Modal
+        isOpen={isFaqModalOpen}
+        onRequestClose={() => setIsFaqModalOpen(false)}
+        contentLabel='faq'
+        className={`flex flex-col items-center w-screen min-h-screen ${gradientBg.gradientBackground}`}
+      >
+        <div className='container py-28 flex flex-col items-center gap-2 relative w-screen h-screen max-h-screen overflow-auto z-10'>
+          <Image src={'/modalContent/shake-faq.svg'} alt={'faq'} width={600} height={300} className='' />
+          <button
+            className='absolute top-32 right-52 bg-gradient-radial from-[#E664BE] to-[#E66484] px-2 rounded-md cursor-pointer text-white shadow-xl hover:filter hover:brightness-125 z-40'
+            onClick={() => setIsFaqModalOpen(false)}
+            title='Close popup'
+          >
+            X
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 }
