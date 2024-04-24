b4cc5ef87904fc6f9eb168133d0252412bd33584
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2024-04-24 15:05:58 +0100

    links replaced

--- src/links.js
+++ src/links.js
@@ -1,4 +1,4 @@
-export const BOT_LINK = 'https://t.me/baseclubio_bot'
-export const DEX_LINK = 'https://dexscreener.com/solana/cht7k3d6hrrfygwxhcseoxz389jdvhpnsty2cy87errl'
-export const TG_LINK = 'https://t.me/baseclubio'
-export const X_LINK ='https://twitter.com/baseclubio'
+export const BOT_LINK = 'https://t.me/solclub_bot'
+export const DEX_LINK = 'https://dexscreener.com/solana/'
+export const TG_LINK = 'https://t.me/solanaclubio'
+export const X_LINK ='https://twitter.com/solanaclubio'
