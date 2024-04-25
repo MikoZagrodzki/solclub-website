a20a8d3022d86803a2f9c214267465b564ad6e21
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2024-04-25 20:28:57 +0100

    contract added

--- src/links.js
+++ src/links.js
@@ -1,4 +1,4 @@
 export const BOT_LINK = 'https://t.me/solclub_bot'
-export const DEX_LINK = 'https://dexscreener.com/solana/'
+export const DEX_LINK = 'https://dexscreener.com/solana/57DTVLoNaaqxG2JPPXYSMGiNbsdhBy3Bx4mBkS7XeCJZ'
 export const TG_LINK = 'https://t.me/solanaclubio'
 export const X_LINK ='https://twitter.com/solanaclubio'
